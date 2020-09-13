1c73850aded2a0b6272159addfc1e8daeb21a1c7
Sanjeev Thapa <sanjeev.gaha@outlook.com>
2020-09-13 11:22:21 +0545

    Header finished designing

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 function Header() {
     return (
@@ -19,22 +20,27 @@ function Header() {
                {/* sign in */}
                 <div className='header__option' >
                     <span className='header__optionLineOne'>Hello Sanjeev</span>
-                    <span className='header__optionLinTwo'>Sign In</span>
+                    <span className='header__optionLineTwo'>Sign In</span>
 
                 </div>
 
                 {/* return and orders */}
                 <div className='header__option' >
                     <span className='header__optionLineOne'>Return</span>
-                    <span className='header__optionLinTwo'>&amp; Orders</span>
+                    <span className='header__optionLineTwo'>&amp; Orders</span>
                     
                 </div>
 
                 {/* your prime */}
                 <div className='header__option' >
                     <span className='header__optionLineOne'>Your</span>
-                    <span className='header__optionLinTwo'>Prime</span>
+                    <span className='header__optionLineTwo'>Prime</span>
                 </div>
+                <div className='header__optionBasket'>
+                    <ShoppingCartIcon />
+                    <span className='header__optionLineTwo header__basketCount'>0</span>
+                </div>
+
 
            </div>
         </div>
