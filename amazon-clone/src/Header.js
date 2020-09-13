cc014be221c17b76fabaa523c3648e22abc78d63
Sanjeev Thapa <sanjeev.gaha@outlook.com>
2020-09-13 10:59:13 +0545

    header css

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
@@ -10,6 +11,7 @@ function Header() {
            <div className="header__search">
                 <input className = 'header__searchInput' type='text'/>
                 {/* search logo */}
+                <SearchIcon className='header__searchIcon' />
            </div>
            
            {/* header nav */}
